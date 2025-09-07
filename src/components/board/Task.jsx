24d51d3abfc18faf0826e9ff3ac9233680c83b3c
DaniBencz <daniel.bencz@gmail.com>
2025-09-07 05:15:49 +0200

    use icon for Edit

--- src/components/board/Task.jsx
+++ src/components/board/Task.jsx
@@ -4,8 +4,8 @@ const Task = ({ task, deleteTask, updateTask, openForm }) => {
 
   return (
     <div className="bg-gray-100 p-4 rounded shadow mb-2">
-      <div className="flex justify-between items-center">
-        <div>
+      <div className="flex justify-between items-center flex-wrap gap-2">
+        <div className="flex items-center gap-1">
           {task.status > 0 && (
             <button
               className="px-2 py-1 text-white bg-blue-500 rounded hover:bg-blue-600 hover:cursor-pointer"
@@ -17,7 +17,7 @@ const Task = ({ task, deleteTask, updateTask, openForm }) => {
 
           {task.status < 2 && (
             <button
-              className="px-2 py-1 ml-1 text-white bg-blue-500 rounded hover:bg-blue-600 hover:cursor-pointer"
+              className="px-2 py-1 text-white bg-blue-500 rounded hover:bg-blue-600 hover:cursor-pointer"
               onClick={moveRight}
             >
               {`${">"}`}
@@ -29,8 +29,9 @@ const Task = ({ task, deleteTask, updateTask, openForm }) => {
           <button
             className="px-2 py-1 text-white bg-blue-500 rounded hover:bg-blue-600 hover:cursor-pointer"
             onClick={() => openForm(task)}
+            title="Edit"
           >
-            Edit
+            <i className="fa-regular fa-pen-to-square"></i>
           </button>
           <button
             className="px-2 py-1 ml-1 text-white bg-red-500 rounded hover:bg-red-700 hover:cursor-pointer"
@@ -40,7 +41,7 @@ const Task = ({ task, deleteTask, updateTask, openForm }) => {
           </button>
         </div>
       </div>
-      <p className="text-gray-800">{task.name}</p>
+      <p className="mt-1 text-gray-800">{task.name}</p>
       <p className="text-gray-800">{task.description}</p>
     </div>
   );
